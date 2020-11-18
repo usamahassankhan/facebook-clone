7cf946c2cbd8e8ab74a7ed488a12783ada89d562
usamahassankhan <usamahassankhan870@gmail.com>
2020-11-18 19:35:28 +0500

    facebook-clone

---
+++ src/Header.js
@@ -0,0 +1,65 @@
+import React from "react";
+import "./Header.css";
+import fb from "./img/download.png";
+import SearchIcon from "@material-ui/icons/Search";
+import HomeIcon from "@material-ui/icons/Home";
+import FlagIcon from "@material-ui/icons/Flag";
+import SubscriptionOutlinedIcon from "@material-ui/icons/SubscriptionsOutlined";
+import SupervisedUserCircleIcon from "@material-ui/icons/SupervisedUserCircle";
+import StorefrontOutlinedIcon from "@material-ui/icons/StorefrontOutlined";
+import { Avatar, IconButton } from "@material-ui/core";
+import AddIcon from "@material-ui/icons/Add";
+import ForumIcon from "@material-ui/icons/Forum";
+import NotificationsActiveIcon from "@material-ui/icons/NotificationsActive";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+function Header() {
+  return (
+    <div className="header">
+      <div className="header_left">
+        <img src={fb} alt="logo" />
+      </div>
+      <div className="header_input">
+        <SearchIcon />
+        <input type="text" />
+      </div>
+      <div className="header_center">
+        <div className="header_option
+        header_option--active">
+          <HomeIcon fontSize="large" />
+        </div>
+        <div className="header_option">
+          <FlagIcon fontSize="large" />
+        </div>
+        <div className="header_option">
+          <SubscriptionOutlinedIcon fontSize="large" />
+        </div>
+        <div className="header_option">
+          <StorefrontOutlinedIcon fontSize="large" />
+        </div>
+        <div className="header_option">
+          <SupervisedUserCircleIcon fontSize="large" />
+        </div>
+      </div>
+      <div className="header_right">
+        <div className="header_info">
+          <Avatar />
+          <h4>usama</h4>
+        </div>
+        <IconButton>
+          <AddIcon />
+        </IconButton>
+        <IconButton>
+          <ForumIcon />
+        </IconButton>
+        <IconButton>
+          <NotificationsActiveIcon />
+        </IconButton>
+        <IconButton>
+          <ExpandMoreIcon />
+        </IconButton>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDjsInBXrUkgaBhC-mInZklBVsgBLUJl1I",
+    authDomain: "facebook-clone-b8cf0.firebaseapp.com",
+    databaseURL: "https://facebook-clone-b8cf0.firebaseio.com",
+    projectId: "facebook-clone-b8cf0",
+    storageBucket: "facebook-clone-b8cf0.appspot.com",
+    messagingSenderId: "443949400486",
+    appId: "1:443949400486:web:fe4f73016026c63b2b8f67",
+    measurementId: "G-B0BZJ6P9HS"
+  };
